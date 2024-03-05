2081e2abdf8ace99963b5ac411342ab37ccc092f
subhash131 <subhashnayak131@gmail.com>
2024-03-05 23:56:23 +0530

    add component: approach

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Approach from "@/components/approach";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main className="w-full h-full">
       <Hero />
+      <Approach />
     </main>
   );
 }
