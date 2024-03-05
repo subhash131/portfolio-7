b3ebdd36da6a403e0a9aa3f12caf73e5ad393bf6
subhash131 <subhashnayak131@gmail.com>
2024-03-06 03:02:50 +0530

    create component: CreativeFrontendDeveloper

---
+++ components/creative-frontend-developer/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreativeFrontendDeveloper = () => {
+  return <div className="w-screen h-screen">CreativeFrontendDeveloper</div>;
+};
+
+export default CreativeFrontendDeveloper;
