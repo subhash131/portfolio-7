5931754d5befcd531da01f3d2515e229ce0c9ec2
subhash131 <subhashnayak131@gmail.com>
2024-03-05 23:54:44 +0530

    add font:antonio

--- fonts/index.tsx
+++ fonts/index.tsx
@@ -1,9 +1,14 @@
-import { Poppins } from "next/font/google";
-import { Inter } from "next/font/google";
+import { Antonio, Poppins, Inter } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
 export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["devanagari"],
+});
+
+export const antonio = Antonio({
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  
 });
