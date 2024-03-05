6edfa95d59304d92ecf88944e5615bf4c2a0fa71
subhash131 <subhashnayak131@gmail.com>
2024-03-05 12:43:03 +0530

    create layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { SparklesCore } from "@/components/hero/particles-container";
+import { inter } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-black overflow-y-scroll overflow-x-hidden min-h-[200vh] relative text-white`}
+      >
+        <SparklesCore className="w-full h-full absolute" particleDensity={10} />
+        {children}
+      </body>
     </html>
   );
 }
