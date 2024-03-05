2e55fda11d5b7dcdc1cd86fdc031fc0f294bd9f4
subhash131 <subhashnayak131@gmail.com>
2024-03-06 02:49:02 +0530

    add custom tailwind classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      borderColor: {
+        "cus-light": "#252526",
+      },
+      borderWidth: {
+        thin: "0.5px",
+      },
     },
   },
   plugins: [],
