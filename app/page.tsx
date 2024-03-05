24c2165a950c25da0f372fa099f4a7ace65d6ad7
subhash131 <subhashnayak131@gmail.com>
2024-03-06 03:03:25 +0530

    add component: CreativeFrontendDeveloper

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Approach from "@/components/approach";
+import CreativeFrontendDeveloper from "@/components/creative-frontend-developer";
 import Experience from "@/components/experience";
 import Hero from "@/components/hero";
 import SmoothScroll from "@/components/scroll/smooth-scroll";
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero />
         <Approach />
         <Experience />
+        <CreativeFrontendDeveloper />
       </main>
     </SmoothScroll>
   );
