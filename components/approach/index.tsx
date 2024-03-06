b678c98c6822b94bf20801d1fc8cf676fa428c98
subhash131 <subhashnayak131@gmail.com>
2024-03-06 17:13:01 +0530

    add animation: Approach

--- components/approach/index.tsx
+++ components/approach/index.tsx
@@ -1,6 +1,7 @@
 import { poppins } from "@/fonts";
 import Link from "next/link";
 import React from "react";
+import Reveal from "../animate/reveal";
 
 const data = [
   {
@@ -36,7 +37,9 @@ const Approach = () => {
       <div className="absolute w-96 h-96 sky_gradient_background z-1 rounded-full blur-3xl" />
       <div className="">
         <div className="w-full h-20 flex items-center justify-between">
-          <p className="text-7xl opacity-90">My Approach</p>
+          <Reveal>
+            <p className="text-7xl opacity-90">My Approach</p>
+          </Reveal>
           <Link
             href="https://www.geeksforgeeks.org/software-development-life-cycle-sdlc/"
             target="_blank"
@@ -82,11 +85,13 @@ const Card = ({
   description: string;
 }) => {
   return (
-    <div className="w-52 h-full flex flex-col gap-4">
-      <h3 className="text-2xl h-20">{title}</h3>
-      <p className={`${poppins.className} text-xs h-24 text-neutral-400`}>
-        {description}
-      </p>
-    </div>
+    <Reveal>
+      <div className="w-52 h-full flex flex-col gap-4">
+        <h3 className="text-2xl h-20 ">{title}</h3>
+        <p className={`${poppins.className} text-xs h-24 text-neutral-400`}>
+          {description}
+        </p>
+      </div>
+    </Reveal>
   );
 };
