42a8529e90b1904d4b50865dab36eca8344f9ab3
subhash131 <subhashnayak131@gmail.com>
2024-03-06 17:13:42 +0530

    add animation: Footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,51 +1,58 @@
 "use client";
 import React from "react";
 import SpinningText from "../hero/spinning-text";
+import Reveal from "../animate/reveal";
 
 const Footer = () => {
-  const date = new Date();
-
   return (
     <div className="w-screen h-screen px-20 py-10 grid grid-cols-12 grid-rows-12 overflow-hidden bg-red-20 ">
       <div className="row-start-1 row-end-11 col-start-1 col-end-6 flex justify-center items-center flex-col gap-4">
-        <p className="text-6xl">
-          <span className="text-neutral-400">Let&apos;s</span> <br />
-          Connect
-        </p>
         <a
           href="mailto:subhashnayak131@gmail.com"
-          className="text-neutral-500 hover:underline hover:text-[#CA9754] transition-all"
+          className="text-neutral-500 hover:text-[#CA9754] transition-all"
         >
-          subhashnayak131@gmail.com
+          <Reveal className="w-fit">
+            <p className="text-6xl p-1">
+              <span className="text-neutral-400">Let&apos;s</span> <br />
+              Connect
+            </p>
+          </Reveal>
+          <p className="py-1 px-3">subhashnayak131@gmail.com</p>
         </a>
       </div>
       <div
-        className="row-start-4 col-start-7 row-span-3 col-span-2 cursor-pointer"
+        className="row-start-4 col-start-7 row-span-3 col-span-2 cursor-pointer hover:scale-105 transition-all"
         onClick={() => {
           scrollTo({ top: 0, behavior: "smooth" });
         }}
       >
-        <p className="text-xl">Home</p>
+        <Reveal className="w-fit">
+          <p className="text-xl">Home</p>
+        </Reveal>
         <p className="text-xs text-neutral-500">Navigate to top.</p>
       </div>
       <div
-        className="row-start-7 col-start-7 row-span-3 col-span-2 cursor-pointer"
+        className="row-start-7 col-start-7 row-span-3 col-span-2 cursor-pointer hover:scale-105 transition-all"
         onClick={() => {
           scrollTo({ top: innerHeight, behavior: "smooth" });
         }}
       >
-        <p className="text-xl">Process</p>
+        <Reveal className="w-fit">
+          <p className="text-xl">Process</p>
+        </Reveal>
         <p className="text-xs text-neutral-500">
           Take a look at my approach to work.
         </p>
       </div>
       <div
-        className="row-start-7 col-start-10 row-span-3 col-span-2 cursor-pointer"
+        className="row-start-7 col-start-10 row-span-3 col-span-2 cursor-pointer hover:scale-105 transition-all"
         onClick={() => {
           scrollTo({ top: innerHeight * 2, behavior: "smooth" });
         }}
       >
-        <p className="text-xl">Experience</p>
+        <Reveal className="w-fit">
+          <p className="text-xl">Experience</p>
+        </Reveal>
         <p className="text-xs text-neutral-500">Check out my work history.</p>
       </div>
       <div className="row-start-10 row-span-2 col-start-2 col-span-2 grid place-content-center">
