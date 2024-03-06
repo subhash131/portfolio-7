0359dbf42e5c1080ab8513649fc894fb8947724b
subhash131 <subhashnayak131@gmail.com>
2024-03-06 17:13:17 +0530

    add animation: CreativeFrontendDeveloper

--- components/creative-frontend-developer/index.tsx
+++ components/creative-frontend-developer/index.tsx
@@ -1,28 +1,54 @@
 "use client";
 import { manrope } from "@/fonts";
-import { motion, useScroll } from "framer-motion";
-import React, { useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import Reveal from "../animate/reveal";
 
 const CreativeFrontendDeveloper = () => {
   const containerRef = useRef(null);
+  const inView = useInView(containerRef, { once: true });
+  const mainControls = useAnimation();
+
+  const leftVariants = {
+    initial: {
+      x: "-300%",
+    },
+    final: {
+      x: "-40%",
+    },
+  };
+  const rightVariants = {
+    initial: {
+      x: "200%",
+    },
+    final: {
+      x: "40%",
+    },
+  };
+  useEffect(() => {
+    if (inView) mainControls.start("final");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
 
   return (
     <div className="w-screen h-[70vh] py-10" ref={containerRef}>
       <div className="h-28 px-20 ">
-        <p className="w-72 text-xs text-neutral-400">
-          I&apos;ve worked with the Fortune 500 brands, specializing in
-          implementing the solutions for large-scale applications. This
-          experience has provided me with a deep understanding of design and the
-          skill to facilitate effective communication within teams.
-        </p>
+        <Reveal className="w-fit">
+          <p className="w-72 text-xs text-neutral-400">
+            I&apos;ve worked with the Fortune 500 brands, specializing in
+            implementing the solutions for large-scale applications. This
+            experience has provided me with a deep understanding of design and
+            the skill to facilitate effective communication within teams.
+          </p>
+        </Reveal>
       </div>
       <div
         className={`h-72 w-full ${manrope.className} text-8xl relative flex flex-col items-center  `}
       >
         <motion.div
-          className="w-fit ml-0 "
-          style={{ x: "-150%" }}
-          whileInView={{ x: "-50%" }}
+          variants={leftVariants}
+          animate={mainControls}
+          initial="initial"
           transition={{ delay: 0.5, duration: 0.5 }}
         >
           <p className="italic">Creative</p>
@@ -31,10 +57,10 @@ const CreativeFrontendDeveloper = () => {
           <p className="italic text-neutral-300">Frontend</p>
         </div>
         <motion.div
-          className="mr-0 w-fit"
-          style={{ x: "120%" }}
-          whileInView={{ x: "50%" }}
-          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={rightVariants}
+          animate={mainControls}
+          initial="initial"
+          transition={{ delay: 0.8, duration: 0.5 }}
         >
           <p className="italic">Developer</p>
         </motion.div>
