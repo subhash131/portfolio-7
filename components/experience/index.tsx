70028026da109920c28f0ecd2a2f42ec572af70f
Subhash Nayak <64570322+subhash131@users.noreply.github.com>
2024-04-05 20:22:41 +0530

    Update experience index.tsx

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import Reveal from "../animate/reveal";
 
 const workHistory = [
-  {
-    title: "Senior Frontend Engineer (Freelance)",
-    company: "Sponsor Sphere (early startup)",
-    location: "Remote",
-    duration: "Jan 2024 - Present",
-    link: "https://sponsor-shpere.vercel.app/",
-  },
+  // {
+  //   title: "Senior Frontend Engineer (Freelance)",
+  //   company: "Sponsor Sphere (early startup)",
+  //   location: "Remote",
+  //   duration: "Jan 2024 - Present",
+  //   link: "https://sponsor-shpere.vercel.app/",
+  // },
   {
     title: "Software Engineer - II",
     company: "PwC (US)",
